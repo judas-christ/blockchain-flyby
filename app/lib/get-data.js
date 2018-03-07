d56cca764f4d035583cc7e2a5d5ea03eb41ade92
Daniel Hägglund <daniel.hagglund@doberman.co>
2018-03-07 19:11:06 +0100

    fixed fake data path

--- app/lib/get-data.js
+++ app/lib/get-data.js
@@ -3,5 +3,5 @@ import getJson from './get-json'
 export default function() {
   return getJson(
     'https://us-central1-hackday-blockchain.cloudfunctions.net/chain'
-  ).catch(() => getJson('/fake-data.json'))
+  ).catch(() => getJson('./fake-data.json'))
 }
